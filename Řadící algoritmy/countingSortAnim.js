a0dfa2bd194a45fd7460a8bab7928ec79593b98f
DanielLalak <144382792+DanielLalak@users.noreply.github.com>
2024-01-17 09:02:00 +0100

    Add files via upload

--- Řadící algoritmy/countingSortAnim.js
+++ Řadící algoritmy/countingSortAnim.js
@@ -1,26 +1,31 @@
 class CountingSortAnim extends SortAnim {
-    async sortAsc(array) {
-        let max = array[0];
-        for (let i = 1; i < array.length; i++) {
-            if (max < array[i]) max = array[i];
+    async sortAsc() {
+        let max = this.values[0];
+        for (let i = 1; i < this.values.length; i++) {
+            if (max < this.values[i]) max = this.values[i];
         }
         max++;
         let countArray = new Array(max);
         for (let i = 0; i < max; i++) {
             countArray[i] = 0;
         }
-        for (let i = 0; i < array.length; i++) {
-            countArray[array[i]] = 0;
+        for (let i = 0; i < this.values.length; i++) {
+            countArray[this.values[i]]++;
         }
         for (let i = 1; i < max; i++) {
             countArray[i] += countArray[i - 1];
         }
-        let outputArray = new Array(array.length);
-        for (let i = array.length - 1; i >= 0; i--) {
-            outputArray[--countArray[array[i]]] = array[i];
+        let outputArray = new Array(this.values.length);
+        for (let i = this.values.length - 1; i >= 0; i--) {
+            this.setCompareColor(this.columns[i]);
+            await this.sleep(50);
+            this.setDefaultColor(this.columns[i]);
+            outputArray[--countArray[this.values[i]]] = this.values[i];
         }
-        for (let i = 0; i < array.length; i++) {
-            array[i] = outputArray[i];
+        for (let i = 0; i < this.values.length; i++) {
+            this.setValue(i, outputArray[i]);
+            await this.sleep(50);
+            this.setSortedColor(this.columns[i]);
         }
     }
 }
